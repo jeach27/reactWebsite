713b50b4cc0fc2e43086f22ea35c344b5911c1c5
jeach27 <3658279400101@ingenieria.usac.edu.gt>
2024-07-23 22:39:37 -0600

    {main}navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,38 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React from 'react'
-import logo from '../assets/logoP.jpg'
+import React, { useState } from "react";
+import Logo from "../assets/logoP.jpg";
+import { Link } from "react-router-dom";
+import list_alt from '@mui/icons-material'
+import "../styles/Navbar.css";
 
 function Navbar() {
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
   return (
-    <div className='navbar'>
-      <div className='leftSide'></div>
-        <img src={logo} />
-      <div className='rightSide'></div>
+    <div className="navbar">
+      <div className="leftSide" id={openLinks ? "open" : "close"}>
+        <img src={Logo} />
+        <div className="hiddenLinks">
+          <Link to="/"> Home </Link>
+          <Link to="/menu"> Menu </Link>
+          <Link to="/about"> About </Link>
+          <Link to="/contact"> Contact </Link>
+        </div>
+      </div>
+      <div className="rightSide">
+        <Link to="/"> Home </Link>
+        <Link to="/menu"> Menu </Link>
+        <Link to="/about"> About </Link>
+        <Link to="/contact"> Contact </Link>
+        <button onClick={toggleNavbar}>
+          <list_alt></list_alt>
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
